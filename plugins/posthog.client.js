7ce1cee13f6d0b21993ee5362c3f4f5d49c8bbb0
Hamoudy <85179120+dearbornio@users.noreply.github.com>
2025-03-11 13:44:41 -0400

    fix posthog plugin

--- plugins/posthog.client.js
+++ plugins/posthog.client.js
@@ -3,7 +3,8 @@ import posthog from 'posthog-js'
 export default defineNuxtPlugin(nuxtApp => {
   const runtimeConfig = useRuntimeConfig();
   const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
-    api_host: runtimeConfig.public.posthogHost || 'https://us.i.posthog.com',
+    api_host: runtimeConfig.public.posthogHost,
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
     capture_pageview: false, // we add manual pageview capturing below
     loaded: (posthog) => {
       if (import.meta.env.MODE === 'development') posthog.debug();
